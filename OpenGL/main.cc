740384bc6ad027b233c45ff2a015c926078a2b6b
0xBAMA <jon.baker1222@gmail.com>
2019-12-18 12:39:25 -0500

    small fixes

--- OpenGL/main.cc
+++ OpenGL/main.cc
@@ -85,8 +85,8 @@ int main( int argc, char * argv[] )
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 5 );
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
 
-    static const int width = 800;
-    static const int height = 600;
+    static const int width = 1200;
+    static const int height = 700;
 
     SDL_Window * window = SDL_CreateWindow( "OpenGL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
     SDL_GLContext context = SDL_GL_CreateContext( window );
