db9a9b9e38fef0ff56a9ae14088f982f853068aa
jb <jon.baker1222@gmail.com>
2020-04-14 13:07:25 -0400

    remove splash

--- 2d/main.cc
+++ 2d/main.cc
@@ -46,6 +46,30 @@ int main()
       //loop through this process of handling the shit for the keyboard, then drawing the shit that we do in the draw function
 
 
+
+      SDL_Init( SDL_INIT_VIDEO );
+      SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+      SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
+      SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+      SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+      SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+      SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
+
+      SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1);
+      SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 8);
+
+      SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
+      SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 5 );
+      SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+
+
+
+      auto window = SDL_CreateWindow( "OpenGL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 100, 200, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+      auto context = SDL_GL_CreateContext( window );
+
+
+      SDL_Delay(1000);
+
     //Main loop flags
     my_game->quit = false;
 
@@ -56,6 +80,33 @@ int main()
     }
 
     delete my_game;  //this calls the destructor and deallocates all the SDL stuff - there's better ways to do this
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return EXIT_SUCCESS;
 }
